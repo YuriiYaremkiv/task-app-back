bc5bc05517c06cbcda7e4de095156e147e123eb7
yyaremkiv <y.yaremkiv@gmail.com>
2023-05-09 22:59:10 +0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://www.example.com',
-      'http://app.example.com',
-      'https://example.com',
-      'https://www.example.com',
-      'https://app.example.com',
-      'https://events-app-front.vercel.app',
-    ],
+    origin: '*',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
