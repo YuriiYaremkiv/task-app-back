49c32acacf3f7327a1493dc30094ab5ccff24609
yyaremkiv <y.yaremkiv@gmail.com>
2023-05-09 23:14:01 +0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: '*',
-  //   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3000/',
+      'https://localhost:3000',
+      'https://localhost:3000/',
+      'http://www.example.com',
+      'http://app.example.com',
+      'https://example.com',
+      'https://www.example.com',
+      'https://app.example.com',
+      'https://events-app-front.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
