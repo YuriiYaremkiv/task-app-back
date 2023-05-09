0ba9ff48c068cb067c4fe1dbaf6b4caf83a01ded
yyaremkiv <y.yaremkiv@gmail.com>
2023-05-09 23:01:17 +0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import * as cors from 'cors';
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  //   credentials: true,
+  // });
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
